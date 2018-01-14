ee2eafef2d4d097cb579831bdf8306e36ada8859
ragnarockie08 <ragnarockie08@gmail.com>
2018-01-14 14:34:33 +0100

    Dynamic Array implementation

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,82 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
-public class DynamicIntArray {
+import java.util.Arrays;
+
+public class DynamicIntArray<E> {
+
+    private  Object[] data;
+    private int size;
+
+    public DynamicIntArray(){
+        data = new Object [0];
+        size = 0;
+    }
+
+    public DynamicIntArray(int initialSize){
+        data = new Object [initialSize];
+        size = data.length;
+    }
+
+    private void increaseDataSize(int newSize){
+
+        int length = data.length;
+        if (length < newSize){
+            data = Arrays.copyOf(data, newSize);
+        }
+    }
+
+    public E get(int index){
+
+        checkIndex(index);
+        return (E) data[index];
+    }
+
+    public boolean add(E element){
+
+        increaseDataSize(size + 1);
+        data[size++] = element;
+        return true;
+    }
+
+    public boolean remove(int index){
+        checkIndex(index);
+
+        for (int i = index; i < size() - 1; i++) {
+            data[i] = data[i + 1];
+        }
+
+        data = Arrays.copyOf(data, --size);
+
+        return true;
+
+    }
+
+    public boolean insert(int index, E element){
+        checkIndex(index);
+        data[index] = element;
+        return true;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private void checkIndex(int index){
+        if (index >= size || index < 0){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public String toString(){
+        String output = "";
+        for (Object element: data){
+            output += " " + element;
+        }
+        return output;
+    }
 
 }
--- src/main/java/com/codecool/dynamicArrayDojo/Program.java
+++ src/main/java/com/codecool/dynamicArrayDojo/Program.java
@@ -1,8 +1,20 @@
 package com.codecool.dynamicArrayDojo;
 
+
 public class Program {
 
     public static void main(String[] args) {
         System.out.println("Do not run me, run the TESTS!");
+
+        DynamicIntArray<Integer> array = new DynamicIntArray<>();
+
+        array.add(5);
+        array.add(6);
+        array.add(7);
+        System.out.println(array.toString());
+        array.remove(1);
+        System.out.println(array.toString());
+        System.out.println(array.size());
+
     }
 }
